2d0c292d7a797cbf6ae8beb75dd231f6fb0303e7
Sauww <bastien.zigmann@free.fr>
2020-03-12 17:06:38 +0100

    Sources added for gradient 2D

--- shaders/noises/gradient/fragment_shader2D.fs
+++ shaders/noises/gradient/fragment_shader2D.fs
@@ -11,13 +11,14 @@ out vec4 outColor;
 int NUM_OCTAVES = 6;
 
 // Random function is taken with arbitrary values who can be modified here
+// source : https://thebookofshaders.com/edit.php#11/2d-gnoise.frag
 vec2 rand2(vec2 st){
     st = vec2( dot(st,vec2(127.1,311.7)),
               dot(st,vec2(269.5,183.3)) );
     return -1.0 + 2.0*fract(sin(st)*43758.5453123);
 }
 
-// source : https://thebookofshaders.com/edit.php#11/2d-gnoise.frag
+// source : https://www.shadertoy.com/view/XdXGW8
 float gradient(vec2 st) {
     vec2 i = floor(st);
     vec2 f = fract(st);
