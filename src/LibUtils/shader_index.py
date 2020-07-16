e93e74e70d3da44920e3972a8ef52fc1c869daf8
Sauww <bastien.zigmann@free.fr>
2020-07-16 10:08:03 +0200

    Update gitignore

--- src/LibUtils/shader_index.py
+++ src/LibUtils/shader_index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-dictTagToPath = {'FBM_VORONOI': '/src/shader_code/features/fbm_voronoi.fs',
-'PLATEAU': '/src/shader_code/features/plateau.fs',
-'RANDOM_3D': '/src/shader_code/utils/random.fs',
-'NOISE_VORONOI_2D': '/src/shader_code/utils/voronoi.fs',
+dictTagToPath = {'PLATEAU': '/src/shader_code/features/plateau.fs',
 'FBM_GRADIENT': '/src/shader_code/features/fbm_gradient.fs',
+'PHASOR': '/src/shader_code/features/phasor.fs',
+'FBM_VORONOI': '/src/shader_code/features/fbm_voronoi.fs',
+'NOISE_VORONOI_2D': '/src/shader_code/utils/voronoi.fs',
 'RANDOM_2D': '/src/shader_code/utils/random.fs',
-'NOISE_GRADIENT_2D': '/src/shader_code/utils/gradient.fs',
-'PHASOR': '/src/shader_code/features/phasor.fs'}
+'RANDOM_3D': '/src/shader_code/utils/random.fs',
+'NOISE_GRADIENT_2D': '/src/shader_code/utils/gradient.fs'}
 
-dictFeatureFunctionToTag = {'fbm_voronoi': 'FBM_VORONOI',
+dictFeatureFunctionToTag = {'plateau': 'PLATEAU',
 'fbm_gradient': 'FBM_GRADIENT',
-'plateau': 'PLATEAU',
-'phasor': 'PHASOR'}
+'phasor': 'PHASOR',
+'fbm_voronoi': 'FBM_VORONOI'}
